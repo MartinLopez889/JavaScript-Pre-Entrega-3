83ecfd8d8cbc5eeac810b9f7936e58075dce7a9b
Martin Lopez <martin.lopez.889@gmail.com>
2023-08-15 21:30:42 -0300

    agregado el sessionStorage, con el set y getItem, y el json

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
 //PRE-ENTREGA 3
 
 //Creo la lista de estudiantes
-const lista =[
-    {nombre: "martin", grado: "1A", estado: "presente"},
-    {nombre: "sofia", grado: "1A", estado: "ausente"},
-    {nombre: "lucas", grado: "1B", estado: "ausente"},
-    {nombre: "mariana", grado: "1B", estado: "presente"},
-    {nombre: "ignacio", grado: "1A", estado: "presente"},
-    {nombre: "lujan", grado: "1B",  estado: "presente"},
+const lista = JSON.parse(sessionStorage.getItem("listaAlumnos")) || [
+    {nombre: "martin", curso: "javascript", estado: "presente"},
+    {nombre: "sofia", curso: "javascript", estado: "ausente"},
+    {nombre: "lucas", curso: "javascript", estado: "ausente"},
+    {nombre: "mariana", curso: "javascript", estado: "presente"},
+    {nombre: "ignacio", curso: "javascript", estado: "presente"},
+    {nombre: "lujan", curso: "javascript",  estado: "presente"},
 ]
+console.table(lista)
 
 //Utilizo el getElementbyId para traer "formulario-estudiante" del html.
 const formulario = document.getElementById("formulario-estudiante");
 
 //Utilizo el evento addEventListener para que me 'escuche' el evento a la hora de poner "agregar estudiante"
 formulario.addEventListener("submit", function(event){
-  event.preventDefault(); // Para prevenir el envío del formulario
-  const nombre = document.getElementById("nombre").value;
-  const grado = document.getElementById("grado").value;
-  const estado = document.getElementById("estado").value;
+    event.preventDefault(); // Para prevenir el envío del formulario
+    const nombre = document.getElementById("nombre").value;
+    const curso = document.getElementById("curso").value;
+    const estado = document.getElementById("estado").value;
 
-  lista.push({nombre, grado, estado});
-  console.table(lista);
+    //Verifico que el alumno ingresado no esté repetido, y luego utilizo el operador ternario "?" para que me haga un alert esté o no, repetido.
+    const verificarAlumno = lista.find(alumno => alumno.nombre === nombre);
 
-  formulario.reset(); //para que me borre los datos del formulario, una vez ingresados.
-})
+    const mensaje = verificarAlumno ? "Ese alumno ya está registrado" : (lista.push({nombre, curso, estado}), console.table(lista), "Nuevo alumno registrado");
+
+    alert(mensaje);
+    sessionStorage.setItem("listaAlumnos", JSON.stringify(lista))
+    
+    formulario.reset();//para que me borre los datos del formulario, una vez ingresados.
+  }
+)
